b22459804d09c2f5177df5ce878f0283cca0d788
Brandon.Campbell <brcamp13@yahoo.com>
2019-03-25 22:00:32 -0700

    Still not sure why nothing is working

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
 
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
