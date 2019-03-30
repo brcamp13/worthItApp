44e5a5ab19e8e6bb8a6e784b6b0d3960cec14b1d
Brandon.Campbell <brcamp13@yahoo.com>
2019-03-29 18:44:01 -0700

    Committing something to do with second page

--- src/components/SecondPage/SecondPage.js
+++ src/components/SecondPage/SecondPage.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 
 const SecondPage = () => (
 
-    <div>
+    <div style={{
+        verticalAlign: 'center',
+    }}>
         <Grid celled>
 
             <Grid.Row>
