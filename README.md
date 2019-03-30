b2df70b9ae7d1097a925d8e5f53350f8bbd2e984
Brandon.Campbell <brcamp13@yahoo.com>
2019-03-29 18:27:43 -0700

    Preliminary styling done on the second page

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - [ ] Test the entire app flow from beginning to end
 - [ ] Better style the first page
 - [ ] Transition effect when moving between pages
+- [ ] Format for mobile and different-sized devices
 - [ ] Research information for most majors (or the most popular)
 - [ ] Derive an objective metric for determining the worth of a major
 - [ ] Deploy
--- src/components/SecondPage/SecondPage.js
+++ src/components/SecondPage/SecondPage.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Grid, Button }  from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const SecondPage = () => (
 
     <div>
-        <h1>Sup G</h1>
+        <Grid celled>
+
+            <Grid.Row>
+                <Grid.Column width={3}>
+                    <h1>By the numbers:</h1>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                    <p>A bunch of random stuff is here for the time being</p>
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+                <Grid.Column width={3}>
+                    <h1>Our opinion:</h1>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                    <p>More random stuff is here for the time being</p>
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+                <Grid.Column width={16} textAlign="center">
+                    <Link to="/"><Button color='yellow'>Go Back</Button></Link>
+                </Grid.Column>
+            </Grid.Row>
+
+        </Grid>
     </div>
+
 )
 
 export default SecondPage;
