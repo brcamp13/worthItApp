d7c53ae37273a45990ac4553f6caeac33cd6aab1
Brandon.Campbell <brcamp13@yahoo.com>
2019-04-06 15:31:19 -0700

    More styling

--- src/components/SecondPage/SecondPage.js
+++ src/components/SecondPage/SecondPage.js
@@ -18,7 +18,7 @@ const smallTitleStyle = {
 
 const verdictStyle = {
     fontFamily: 'Roboto Slab', 
-    fontSize: '2.2rem',
+    fontSize: '1.5rem',
 }
 
 const majorTextStyle = {
@@ -33,7 +33,24 @@ const majorTextRowStyle = {
 const numberStyle = {
     fontFamily: 'Open Sans', 
     fontSize: '1.1rem', 
-    color: '#FFD700', 
+}
+
+
+const numbersRowStyle = {
+    marginBottom: '1.1rem'
+}
+
+const numberBoxStyle = {
+    borderRadius: '25px',
+    backgroundColor: 'rgba(255, 255, 255, 0.25)',
+    padding: '15px', 
+    width: '175px',
+    height: '130px',
+    marginTop: '1.1rem'
+}
+
+const numberInnerBoxStyle = {
+    marginTop: '1.4rem'
 }
 
 const paragraphStyle = {
@@ -45,6 +62,7 @@ const buttonStyle = {
     margin: '0 auto', 
     display: 'block',
     marginTop: '3rem',
+    marginBottom: '2rem',
     fontFamily: 'Open Sans'
 }
 
@@ -68,7 +86,7 @@ const SecondPage = ({ onGoHomePress, majorSelection }) => {
                 </Fade>
 
                 <Fade top>
-                    <Row>
+                    <Row style={numbersRowStyle}>
                         <Col align='center'>
                             <span style={titleStyle}><strong><em>By The Numbers:</em></strong></span>
                         </Col>
@@ -78,24 +96,40 @@ const SecondPage = ({ onGoHomePress, majorSelection }) => {
                 <Fade top>
                     <Row>
                         <Col xs={12} md={3} align='center'>
-                            <span style={smallTitleStyle}><strong>Annual Grads</strong></span>
-                            <br></br>
-                            <span style={numberStyle}><strong><em>{majorSelection.annualGraduates}</em></strong></span>
+                            <div style={numberBoxStyle}>
+                                <div style={numberInnerBoxStyle}>
+                                    <span style={smallTitleStyle}><strong>Annual Grads:</strong></span>
+                                    <hr></hr>
+                                    <span style={numberStyle}><strong><em>{majorSelection.annualGraduates}</em></strong></span>
+                                </div>
+                            </div>
                         </Col>
                         <Col xs={12} md={3} align='center'>
-                            <span style={smallTitleStyle}><strong>Unemp. Rate</strong></span>
-                            <br></br>
-                            <span style={numberStyle}><strong><em>{majorSelection.unemploymentRate}</em></strong></span>
+                            <div style={numberBoxStyle}>
+                                <div style={numberInnerBoxStyle}>
+                                    <span style={smallTitleStyle}><strong>Unemp. Rate:</strong></span>
+                                    <hr></hr>
+                                    <span style={numberStyle}><strong><em>{majorSelection.unemploymentRate}</em></strong></span>
+                                </div>
+                            </div>
                         </Col>
                         <Col xs={12} md={3} align='center'>
-                            <span style={smallTitleStyle}><strong>Avg. Salary</strong></span>
-                            <br></br>
-                            <span style={numberStyle}><strong><em>{majorSelection.averageSalary}</em></strong></span>
+                            <div style={numberBoxStyle}>
+                                <div style={numberInnerBoxStyle}>
+                                    <span style={smallTitleStyle}><strong>Avg. Salary:</strong></span>
+                                    <hr></hr>
+                                    <span style={numberStyle}><strong><em>{majorSelection.averageSalary}</em></strong></span>
+                                </div>
+                            </div>
                         </Col>
                         <Col xs={12} md={3} align='center'>
-                            <span style={smallTitleStyle}><strong>Sector Growth*</strong></span>
-                            <br></br>
-                            <span style={numberStyle}><strong><em>{majorSelection.industryGrowth}</em></strong></span>
+                            <div style={numberBoxStyle}>
+                                <div style={numberInnerBoxStyle}>
+                                    <span style={smallTitleStyle}><strong>Sector Growth:</strong></span>
+                                    <hr></hr>
+                                    <span style={numberStyle}><strong><em>{majorSelection.industryGrowth}</em></strong></span>
+                                </div>
+                            </div>
                         </Col>
                     </Row>
                 </Fade>
@@ -103,7 +137,7 @@ const SecondPage = ({ onGoHomePress, majorSelection }) => {
                 <hr></hr>
 
                 <Fade top>
-                    <Row>
+                    <Row style={numbersRowStyle}>
                         <Col align='center'>
                             <span style={titleStyle}><strong><em>Our Opinion: </em></strong></span>
                         </Col>
